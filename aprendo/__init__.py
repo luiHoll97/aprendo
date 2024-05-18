fbd61f6f6df90090d14925635cc66efd99cba1d2
Lui Holliday <108282741+luiHoll97@users.noreply.github.com>
2024-05-18 17:16:06 +0100

    update imports

--- aprendo/__init__.py
+++ aprendo/__init__.py
@@ -1,3 +1,4 @@
 
 from aprendo.verbs import RegularVerb, ERRegularVerb, IRRegularVerb, ARRegularVerb
-from aprendo.db import VerbsDB
+from aprendo.db import VerbsDB, NounsDB
+from aprendo.nouns import Noun

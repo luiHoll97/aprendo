96b568328ad8c2b9b5cc8cc016d8479d80d2c81e
Lui Holliday <108282741+luiHoll97@users.noreply.github.com>
2024-05-19 22:16:48 +0100

    version change

--- setup.py
+++ setup.py
@@ -5,21 +5,9 @@ import subprocess
 this_dir = Path(__file__).parent
 long_description = (this_dir / "README.md").read_text()
 
-def get_version_from_git():
-    try:
-        # Get the latest tag from Git
-        version = subprocess.check_output(['git', 'describe', '--tags']).strip().decode('utf-8')
-        # Ensure the version is in the correct format
-        if version.startswith('v'):
-            version = version[1:]
-        return version
-    except subprocess.CalledProcessError:
-        # Handle the case where there are no tags or git is not available
-        return "0.0.0"
-
 setup(
     name='aprendo',
-    version=get_version_from_git(),
+    version="0.0.1",
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='Quieres aprender español? "Aprendo" es la solución! Add, edit, conjugate, learn and test your Spanish verbs and vocab.',
